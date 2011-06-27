6e86508cb8f5056b54044f9a187aa601881caf86
jr <jirialmovie@gmail.com>
2011-06-27 19:19:31 +0200

    First Character's accessors

--- character.cpp
+++ character.cpp
@@ -61,3 +61,133 @@ jlug::Character::Character(unsigned int cid, const std::string& cname, unsigned 
 jlug::Character::Character(unsigned int cid, const std::string& cname, unsigned int posx, unsigned int posy, unsigned int posz, jlug::ImageManager& imageM):
                         charset(cid), name(cname), x(posx), y(posy), z(posz), IM(imageM)
 {}
+
+
+/**
+* \brief set manually an X-position
+*/
+
+void jlug::Character::setX(unsigned int posx)
+  {
+      x = posx;
+  }
+
+/**
+* \brief set manually an Y-position
+*/
+
+void jlug::Character::setY(unsigned int posy)
+ {
+     y = posy;
+ }
+
+/**
+* \brief set manually a Z-position
+*/
+
+void jlug::Character::setZ(unsigned int posz)
+ {
+     z = posz;
+ }
+
+
+/**
+* \brief set manually a 2D position
+*/
+void jlug::Character::setPosition(unsigned int posx, unsigned int posy)
+ {
+     setX(posx);
+     setY(posy);
+ }
+
+/**
+* \brief set manually a 3D position
+*/
+
+void jlug::Character::setPosition(unsigned int posx, unsigned int posy, unsigned int posz)
+ {
+     setPosition(posx, posy);
+     setZ(posz);
+ }
+
+/**
+* \brief set manually a charset
+*/
+
+void jlug::Character::setCharset(unsigned int cid)
+ {
+     charset = cid;
+ }
+
+/**
+* \brief set manually a name
+*/
+
+void jlug::Character::setName(const std::string& cname)
+ {
+     name = cname;
+ }
+
+
+
+
+/**
+* \brief get character's X-position
+* \return X-position
+*/
+
+unsigned int jlug::Character::getX(void)
+ {
+     return x;
+ }
+
+/**
+* \brief get character's Y-position
+* \return Y-position
+*/
+
+unsigned int jlug::Character::getY(void)
+ {
+     return y;
+ }
+
+/**
+* \brief get character's Z-position
+* \return Z-position
+*/
+
+unsigned int jlug::Character::getZ(void)
+ {
+     return z;
+ }
+
+/**
+* \brief get character's 2D position
+* \return 2D position in a Rect
+*/
+
+jlug::Rect jlug::Character::getPosition(void)
+ {
+     jlug::Rect position = {x, y, 0, 0};
+     return position;
+ }
+
+/**
+* \brief get character's charset
+* \return charset's ID
+*/
+
+unsigned int jlug::Character::getCharset(void)
+ {
+     return charset;
+ }
+
+/**
+* \brief get character's name
+* \return name in a std::string
+*/
+
+const std::string& jlug::Character::getName(void)
+ {
+     return name;
+ }
--- character.hpp
+++ character.hpp
@@ -31,6 +31,22 @@ namespace jlug
             Character(unsigned int cid, const std::string& cname, unsigned int posx, unsigned int posy, unsigned int posz, jlug::ImageManager& imageM);
             ~Character(void);
 
+            void setX(unsigned int posx);
+            void setY(unsigned int posy);
+            void setZ(unsigned int posz);
+            void setPosition(unsigned int posx, unsigned int posy);
+            void setPosition(unsigned int posx, unsigned int posy, unsigned int posz);
+            void setCharset(unsigned int cid);
+            void setName(const std::string& cname);
+
+            unsigned int getX(void);
+            unsigned int getY(void);
+            unsigned int getZ(void);
+            jlug::Rect getPosition(void);
+            unsigned int getCharset(void);
+            const std::string& getName(void);
+
+
 
         private:
             unsigned int charset; /*!< ID of the character's charset */

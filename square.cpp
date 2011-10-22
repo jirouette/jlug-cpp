60abfb036628653effe102649a1453d068b49a61
jr <jirialmovie@gmail.com>
2011-10-22 16:56:57 +0200

    Add methods to manually change Square's vertex

--- square.cpp
+++ square.cpp
@@ -1,10 +1,38 @@
 #include "square.hpp"
 
 jlug::Square::Square(void):Shape()
-{}
+{
+    downerLeftCorner.x = 0.f;
+    downerLeftCorner.y = 0.f;
+    downerLeftCorner.z = 0.f;
+
+    downerRightCorner.x = 1.f;
+    downerRightCorner.y = 0.f;
+    downerRightCorner.z = 0.f;
+
+    upperLeftCorner.x = 0.f;
+    upperLeftCorner.y = 1.f;
+    upperLeftCorner.z = 0.f;
+
+    upperRightCorner.x = 1.f;
+    upperRightCorner.y = 1.f;
+    upperRightCorner.z = 0.f;
+}
 
 jlug::Square::~Square(void)
 {}
+
+void jlug::Square::setVertex(bool rightSide, bool upSide, const jlug::Point& vertex)
+{
+    if (rightSide && upSide)
+        upperRightCorner = vertex;
+    else if (rightSide && !upSide)
+        downerRightCorner = vertex;
+    else if (!rightSide && upSide)
+        upperLeftCorner = vertex;
+    else if (!rightSide && !upSide)
+        downerLeftCorner = vertex;
+}
 
 void jlug::Square::draw(void)
 {
@@ -28,10 +56,10 @@ void jlug::Square::draw(void)
 
      glBegin(GL_QUADS);
         glColor3f(color.x, color.y, color.z);
-        if (texture) { glTexCoord2f(XPOS, MAXYPOS); }       glVertex3f(0.f, 0.f, 0.f);
-        if (texture) { glTexCoord2f(XPOS, YPOS); }          glVertex3f(0.f, 1.f, 0.f);
-        if (texture) { glTexCoord2f(MAXXPOS, YPOS); }       glVertex3f(1.f, 1.f, 0.f);
-        if (texture) { glTexCoord2f(MAXXPOS, MAXYPOS); }    glVertex3f(1.f, 0.f, 0.f);
+        if (texture) { glTexCoord2f(XPOS, MAXYPOS); }       glVertex3f(downerLeftCorner.x, downerLeftCorner.y, downerLeftCorner.z);
+        if (texture) { glTexCoord2f(XPOS, YPOS); }          glVertex3f(upperLeftCorner.x, upperLeftCorner.y, upperLeftCorner.z);
+        if (texture) { glTexCoord2f(MAXXPOS, YPOS); }       glVertex3f(upperRightCorner.x, upperRightCorner.y, upperRightCorner.z);
+        if (texture) { glTexCoord2f(MAXXPOS, MAXYPOS); }    glVertex3f(downerRightCorner.x, downerRightCorner.y, downerRightCorner.z);
      glEnd();
 
      if (texture)
--- square.hpp
+++ square.hpp
@@ -10,8 +10,15 @@ namespace jlug
         public:
             Square(void);
             ~Square(void);
+            void setVertex(bool rightSide, bool upSide, const jlug::Point& vertex);
 
             virtual void draw(void);
+
+        protected:
+            jlug::Point upperLeftCorner;
+            jlug::Point upperRightCorner;
+            jlug::Point downerLeftCorner;
+            jlug::Point downerRightCorner;
     };
 }
 
